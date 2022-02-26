1f2035d841c0507d9a9834474cc81facb5ada71a
ErSongHeng <100456048+ErSongHeng@users.noreply.github.com>
2022-02-26 18:51:55 +0800

    Add files via upload
    3rd commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from telegram.ext import Updater
+from datetime import *
+import calendar
 
 updater = Updater(token='5207140517:AAGgW7Ct8NGrjL9oUpdiRNssmVqPKP1KaV4', use_context=True)
 
@@ -27,71 +29,55 @@ updater.start_polling()
 
 ##Add command
 def add(update: Update, context: CallbackContext):
-    textToReturn = "Added: "
-    try:
-        textToReturn = textToReturn + update.message.text[5:]
-
-
-        textSplitBySpace = update.message.text[5:].split()
-        if len(textSplitBySpace) > 3:
-            raise Exception("Too many parameters")
-        if not textSplitBySpace[1].isdigit():
-            raise Exception("Not a time")
-        if textSplitBySpace[0].lower() not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday',
-                                               'sunday']:
-            raise Exception("Not day")
-
-        json_adder(update.message.text[5:])
-    except Exception as e:
-        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
-
+    textToReturn = "Added " + update.message.text[5:]
+    json_adder(update.message.text[5:])
     context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
 
 
+##Sets the alarm, alerts every hour at 00 minutes
+def set(update: Update, context: CallbackContext):
+    while 1:
+        with open("storage.json") as file:
+            content = json.load(file)
+
+        for item in content["reminder"]:
+            index = 1
+            if time_getter()[0] == item['day'] and time_getter()[1] == item['time'][:2]:
+                context.bot.send_message(chat_id=update.effective_chat.id, text=item['link'])
+            else:
+                index+=1
+        dt = datetime.now() + timedelta(hours=1)
+        dt = dt.replace(minute = 0)
+
+        while datetime.now() < dt:
+            time.sleep(1)
+        
+
+set_handler = CommandHandler('set', set, pass_args=True)
 add_handler = CommandHandler('add', add, pass_args=True)
+
 dispatcher.add_handler(add_handler)
+dispatcher.add_handler(set_handler)
 
 import json
-
 
 def json_adder(string):
     splitBySpace = string.split()
     with open("storage.json", "r+") as file:
         content = json.load(file)
-        new_object = {"day": splitBySpace[0], "time": splitBySpace[1], "link": splitBySpace[2]}
+        new_object = {"day": splitBySpace[0], "time":splitBySpace[1], "link":splitBySpace[2]}
         print(new_object)
         content["reminder"].insert(0, new_object)
         file.seek(0)
         json.dump(content, file)
         file.truncate()
 
-
-def listing(update: Update, context: CallbackContext):
-    textToReturn = ""
-
-    try:
-        with open("storage.json") as file:
-            content = json.load(file)
-
-        index = 1
-        for item in content["reminder"]:
-            tempString = str(index) + ". "
-            tempString = tempString + item['day'] + " "
-            tempString = tempString + item['time'] + " "
-            tempString = tempString + item['link']
-            textToReturn = textToReturn + tempString + "\n"
-            index+=1
-    except Exception as e:
-        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
-
-    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
-
-
-
-list_handler = CommandHandler('list', listing)
-dispatcher.add_handler(list_handler)
-
-##json deletor
+##Gets current time
+def time_getter():
+    day = calendar.day_name[date.today().weekday()]
+    hour = datetime.now().strftime("%H")
+    return (day,hour)
+    
 def json_deletor(ind):
 
     with open("storage.json", "r+") as file:
@@ -134,3 +120,27 @@ def delete(update: Update, context: CallbackContext):
 delete_handler = CommandHandler('delete', delete, pass_args=True)
 dispatcher.add_handler(delete_handler)
 
+def listing(update: Update, context: CallbackContext):
+    textToReturn = ""
+
+    try:
+        with open("storage.json") as file:
+            content = json.load(file)
+
+        index = 1
+        for item in content["reminder"]:
+            tempString = str(index) + ". "
+            tempString = tempString + item['day'] + " "
+            tempString = tempString + item['time'] + " "
+            tempString = tempString + item['link']
+            textToReturn = textToReturn + tempString + "\n"
+            index+=1
+    except Exception as e:
+        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
+
+
+
+list_handler = CommandHandler('list', listing)
+dispatcher.add_handler(list_handler)
