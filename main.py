3243bfbdddde9b0662ee2ea88bd339e0538f555f
ErSongHeng <100456048+ErSongHeng@users.noreply.github.com>
2022-02-26 22:55:18 +0800

    Add files via upload
    commit 7?

--- main.py
+++ main.py
@@ -1,28 +1,23 @@
-from telegram.ext import Updater
+from telegram.ext import *
+import logging
+from telegram import Update
+import json
+import random
 
 updater = Updater(token='5207140517:AAGgW7Ct8NGrjL9oUpdiRNssmVqPKP1KaV4', use_context=True)
 
 dispatcher = updater.dispatcher
 
-import logging
-
 logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
                     level=logging.INFO)
 
-from telegram import Update
-from telegram.ext import CallbackContext
-
 
 def start(update: Update, context: CallbackContext):
-    context.bot.send_message(chat_id=update.effective_chat.id, text="fuck u!")
+    context.bot.send_message(chat_id=update.effective_chat.id, text="Hello!")
 
-
-from telegram.ext import CommandHandler
 
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
-
-
 
 
 ##Add command
@@ -38,19 +33,17 @@ def add(update: Update, context: CallbackContext):
             raise Exception("Not a time")
         if textSplitBySpace[0].lower() not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday',
                                                'sunday']:
-            raise Exception("Not day")
+            raise Exception("Not a day")
 
         json_adder(update.message.text[5:])
     except Exception as e:
-        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
+        textToReturn = "Use the format of [Day] [Time] [Link], for example: monday 1600 google.com"
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
 
 
 add_handler = CommandHandler('add', add, pass_args=True)
 dispatcher.add_handler(add_handler)
-
-import json
 
 
 def json_adder(string):
@@ -151,33 +144,23 @@ def json_loader():
                 textToReturn = textToReturn + tempString
         return textToReturn
 
+compliments = ['Have a great day!', 'I like your style!', "You're a smart cookie!", 'I am so proud of you, and I hope you are too!', "On a scale from 1 to 10, you're an 11.", "You're one of a kind.", 'You deserve a hug right now', 'Everyone gets knocked down sometimes; only people like you get back up again and keep going', 'You were cool way before hipsters were cool', 'You light up the room']
+
 ##notify command
 def notify(update: Update, context: CallbackContext):
+    
     textToReturn = "Your links for today: \n"
     try:
-        textToReturn = textToReturn + json_loader()
+        textToReturn = textToReturn + json_loader() + " \n"
 
     except Exception as e:
         print(e)
-        textToReturn = "wrong format lah chee bye"
+        textToReturn = "Wrong format!"
 
-    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn + compliments[random.randint(0,9)])
 
 
 notify_handler = CommandHandler('notify', notify)
 dispatcher.add_handler(notify_handler)
-
-import random
-insults = [' is a fucking retard', ' is a fucking fuck', ' is a fucking dick', ' is a fucking cocksucker', ' is a fucking dickwad', ' is a fucking pussy', ' is a fucking his mom']
-##notify command
-def insult(update: Update, context: CallbackContext):
-    random.randint(0, 6)
-    textToReturn = update.message.text[8:] + " is a fucking " + insults[random.randint(0, 6)]
-
-    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
-
-
-insult_handler = CommandHandler('insult', insult, pass_args=True)
-dispatcher.add_handler(insult_handler)
 
 updater.start_polling()
