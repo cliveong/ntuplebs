492006a9c6dee8ad76938796445203ed5c887a6f
cliveong <69676081+cliveong@users.noreply.github.com>
2022-02-26 22:21:50 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,7 +1,4 @@
 from telegram.ext import Updater
-from datetime import *
-import calendar
-import schedule
 
 updater = Updater(token='5207140517:AAGgW7Ct8NGrjL9oUpdiRNssmVqPKP1KaV4', use_context=True)
 
@@ -25,64 +22,80 @@ from telegram.ext import CommandHandler
 start_handler = CommandHandler('start', start)
 dispatcher.add_handler(start_handler)
 
-updater.start_polling()
+
 
 
 ##Add command
 def add(update: Update, context: CallbackContext):
-    textToReturn = "Added " + update.message.text[5:]
-    json_adder(update.message.text[5:])
+    textToReturn = "Added: "
+    try:
+        textToReturn = textToReturn + update.message.text[5:]
+
+        textSplitBySpace = update.message.text[5:].split()
+        if len(textSplitBySpace) > 3:
+            raise Exception("Too many parameters")
+        if not textSplitBySpace[1].isdigit():
+            raise Exception("Not a time")
+        if textSplitBySpace[0].lower() not in ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday',
+                                               'sunday']:
+            raise Exception("Not day")
+
+        json_adder(update.message.text[5:])
+    except Exception as e:
+        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
+
     context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
 
 
-##Sets the alarm, alerts every hour at 00 minutes
-def set(update: Update, context: CallbackContext):
-    def temp():
-        with open("storage.json") as file:
-            content = json.load(file)
-
-        for item in content["reminder"]:
-            index = 1
-            if time_getter()[0] == item['day'] and time_getter()[1] == item['time'] and datetime.now().strftime('%M,%S')=='00,00':
-                context.bot.send_message(chat_id=update.effective_chat.id, text=item['link'])
-            else:
-                index+=1
-    while True:
-        return temp()
-
-set_handler = CommandHandler('set', set, pass_args=True)
 add_handler = CommandHandler('add', add, pass_args=True)
-
 dispatcher.add_handler(add_handler)
-dispatcher.add_handler(set_handler)
-
 
 import json
+
 
 def json_adder(string):
     splitBySpace = string.split()
     with open("storage.json", "r+") as file:
         content = json.load(file)
-        new_object = {"day": splitBySpace[0], "time":splitBySpace[1], "link":splitBySpace[2]}
+        new_object = {"day": splitBySpace[0], "time": splitBySpace[1], "link": splitBySpace[2]}
         print(new_object)
         content["reminder"].insert(0, new_object)
         file.seek(0)
         json.dump(content, file)
         file.truncate()
 
-##Gets current time
-def time_getter():
-    day = calendar.day_name[date.today().weekday()]
-    hour = datetime.now().strftime("%H%M")
-    return (day,hour)
-    
-def json_deletor(ind):
 
+def listing(update: Update, context: CallbackContext):
+    textToReturn = ""
+
+    try:
+        with open("storage.json") as file:
+            content = json.load(file)
+
+        index = 1
+        for item in content["reminder"]:
+            tempString = str(index) + ". "
+            tempString = tempString + item['day'] + " "
+            tempString = tempString + item['time'] + " "
+            tempString = tempString + item['link']
+            textToReturn = textToReturn + tempString + "\n"
+            index += 1
+    except Exception as e:
+        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
+
+
+list_handler = CommandHandler('list', listing)
+dispatcher.add_handler(list_handler)
+
+
+##json deletor
+def json_deletor(ind):
     with open("storage.json", "r+") as file:
-        print('fuck')
         content = json.load(file)
         tempList = content["reminder"]
-        tempItem = tempList.pop(ind-1);
+        tempItem = tempList.pop(ind - 1);
         tempString = tempItem['day'] + " "
         tempString = tempString + tempItem['time'] + " "
         tempString = tempString + tempItem['link']
@@ -92,8 +105,6 @@ def json_deletor(ind):
         file.truncate()
         print(tempString)
         return tempString
-
-
 
 
 ##Delete command
@@ -118,27 +129,55 @@ def delete(update: Update, context: CallbackContext):
 delete_handler = CommandHandler('delete', delete, pass_args=True)
 dispatcher.add_handler(delete_handler)
 
-def listing(update: Update, context: CallbackContext):
-    textToReturn = ""
+from datetime import datetime
 
-    try:
-        with open("storage.json") as file:
-            content = json.load(file)
+days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
 
+def json_loader():
+    with open("storage.json", "r+") as file:
+        content = json.load(file)
+        curr_date = str(datetime.now().weekday()).lower()
+        curr_day = days[int(curr_date)].lower()
+        print(curr_date)
         index = 1
+        textToReturn = ""
         for item in content["reminder"]:
-            tempString = str(index) + ". "
-            tempString = tempString + item['day'] + " "
-            tempString = tempString + item['time'] + " "
-            tempString = tempString + item['link']
-            textToReturn = textToReturn + tempString + "\n"
-            index+=1
+            if curr_day == item['day']:
+                print()
+                tempString = str(index) + ". "
+                tempString = tempString + item['time'] + " "
+                tempString = tempString + item['link'] + "\n"
+                index += 1
+                textToReturn = textToReturn + tempString
+        return textToReturn
+
+##notify command
+def notify(update: Update, context: CallbackContext):
+    textToReturn = "Your links for today: \n"
+    try:
+        textToReturn = textToReturn + json_loader()
+
     except Exception as e:
-        textToReturn = "Use the format of [Day] [Time] [Link], for example: Monday 1600 google.com"
+        print(e)
+        textToReturn = "wrong format lah chee bye"
 
     context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
 
 
+notify_handler = CommandHandler('notify', notify)
+dispatcher.add_handler(notify_handler)
 
-list_handler = CommandHandler('list', listing)
-dispatcher.add_handler(list_handler)
+import random
+insults = [' is a fucking retard', ' is a fucking fuck', ' is a fucking dick', ' is a fucking cocksucker', ' is a fucking dickwad', ' is a fucking pussy', ' is a fucking his mom']
+##notify command
+def insult(update: Update, context: CallbackContext):
+    random.randint(0, 6)
+    textToReturn = update.message.text[8:] + " is a fucking " + insults[random.randint(0, 6)]
+
+    context.bot.send_message(chat_id=update.effective_chat.id, text=textToReturn)
+
+
+insult_handler = CommandHandler('insult', insult, pass_args=True)
+dispatcher.add_handler(insult_handler)
+
+updater.start_polling()
